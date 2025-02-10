312b688cfd8d7fb407976e00206300a7c4dfe6a9
MHoangSE <hoangtrieultvp@gmail.com>
2025-02-11 00:15:44 +0700

    video 27

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,16 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
+const App = () => {
   const [count, setCount] = useState(0)
+  /**
+   * Logs a greeting message to the console.
+   */
+  const myFunction = () => {
+    console.log('hello'); // Output 'hello' to the console
+  }
 
+  myFunction();
   return (
     <>
       <div> // hot reloading
