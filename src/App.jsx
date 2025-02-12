b2ee41b47a5aa3b52ef8658ca2c9095cf05b371c
MHoangSE <hoangtrieultvp@gmail.com>
2025-02-12 22:48:19 +0700

    video 34

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,21 @@ import TodoData from "./Components/todo/TodoData"
 import TodoNew from "./Components/todo/TodoNew"
 import reactlogo from "./assets/react.svg"
 const App = () => {
+  const hoidanit = "Eric";
+  const age = "25"
+  const data = {
+    address: "Hanoi",
+    country: "Vietnam"
+  }
   return (
     <div className="todo-container">
       <div className="todo-title">Todo List</div>
       <TodoNew />
-      <TodoData />
+      <TodoData
+        name={hoidanit}
+        age={age}
+        data={data}
+      />
       <div className="todo-image">
         <img src={reactlogo} className="logo" ></img>
       </div>
--- src/Components/todo/TodoData.jsx
+++ src/Components/todo/TodoData.jsx
@@ -1,6 +1,18 @@
-const TodoData = () => {
+const TodoData = (props) => {
+    //props is an object{}
+    //{
+    //     name: 'Eric',
+    //     age: 25,
+    //     data: {
+    //         address: 'Hanoi',
+    //         country: 'Vietnam'
+    //     }
+    // }
+    const { name, data, age } = props;
+    console.log("Check props:", props)
     return (
         <div className="todo-data">
+            <div>My name is {age}</div>
             <div>Learning React</div>
             <div>Watching React</div>
         </div>
