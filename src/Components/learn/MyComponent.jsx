61fb7cdef6ec6cd7b526d6df41b0bbb9e31b7c53
MHoangSE <hoangtrieultvp@gmail.com>
2025-02-12 10:46:58 +0700

    Video 31

--- src/Components/learn/MyComponent.jsx
+++ src/Components/learn/MyComponent.jsx
@@ -3,9 +3,16 @@
 
 import './style.css'
 const MyComponent = () => {
+    // const hoidanit = 'Eric 1'; //string
+    // const hoidanit = 25; //number
+    // const hoidanit = undefined; //number
+    // const hoidanit = null;
+    // const hoidanit = [1, 2, 3]
+    const hoidanit = { name: 'Eric', age: 25 }
     return (
         <>
-            <div>Hello World</div>
+            <div>Hello {JSON.stringify(hoidanit)}</div>
+            <div>{console.log(hoidanit.name)}</div>
             <div className="child"
                 style={{ border: '1px solid red', padding: '10px' }}
             >React</div>
